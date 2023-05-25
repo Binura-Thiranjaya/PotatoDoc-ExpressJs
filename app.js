694643474c9304ea50eaa0922338fbd443705269
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-05-25 23:22:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from "cors";
+import bodyParser from "body-parser";
+
+//json
+import Treatments from "./Data/treatments.json" assert { type: "json" };
+
+//Routes
+
+//
+const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/Early_Blight', (req, res) => {
+    // res.send('Welcome to SMILE GAME')
+        res.json(Treatments.diseases[0].treatments)
+    }
+)
+app.get('/Late_Blight', (req, res) => {
+    // res.send('Welcome to SMILE GAME')
+    res.json(Treatments.diseases[1].treatments)
+    }
+)
+
+
+
+
+export default app
