5fc7386f93f6058a55e4f54c9d890432ebfb98dc
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2023-06-08 19:55:17 +0530

    add disease management information

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 
 //json
 import Treatments from "./Data/treatments.json" assert { type: "json" };
+import DiseaseManagement from "./Data/diseaseManagement.json" assert { type: "json" };
 
 //Routes
 
@@ -13,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+app.get('/disease', (req, res) => {
+        res.json(DiseaseManagement.diseases)
+    }
+)
 app.get('/Early_Blight', (req, res) => {
     // res.send('Welcome to SMILE GAME')
         res.json(Treatments.diseases[0].treatments)
